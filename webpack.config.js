d1c3380efbb06e0781e272e3e6545d6656dfa822
guitarplrc <guitarplrc@yahoo.com>
2017-09-30 15:14:58 -0500

    optimize with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const webpack = require('webpack');
 
-var config = {
+let config = {
 	entry: './app/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
@@ -12,8 +13,8 @@ var config = {
 	module: {
 		rules: [
 			{ test: /\.(js)$/, use: 'babel-loader' },
-			{ test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
-			{ test: /\.(gif|png|jpe?g|svg)$/i, use: 'file-loader'}
+			{ test: /\.css$/, use: ['style-loader', 'css-loader'] },
+			{ test: /\.(gif|png|jpe?g|svg)$/i, use: 'file-loader' }
 		]
 	},
 	devServer: {
@@ -21,20 +22,51 @@ var config = {
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: "app/index.html"
+			template: 'app/index.html',
+			minify: {
+				removeComments: true,
+				collapseWhitespace: true,
+				removeRedundantAttributes: true,
+				useShortDoctype: true,
+				removeEmptyAttributes: true,
+				removeStyleLinkTypeAttributes: true,
+				keepClosingSlash: true,
+				minifyJS: true,
+				minifyCSS: true,
+				minifyURLs: true
+			}
 		})
 	]
-}
+};
 
 if (process.env.NODE_ENV === 'production') {
 	config.plugins.push(
 		new webpack.DefinePlugin({
 			'process.env': {
-				'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+				NODE_ENV: JSON.stringify(process.env.NODE_ENV)
 			}
 		}),
-		new webpack.optimize.UglifyJsPlugin()
-	)
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				screw_ie8: true,
+				warnings: false
+			},
+			mangle: {
+				screw_ie8: true
+			},
+			output: {
+				comments: false,
+				screw_ie8: true
+			}
+		}),
+		new CompressionPlugin({
+			asset: '[path].gz[query]',
+			algorithm: 'gzip',
+			test: /\.js$|\.css$|\.html$/,
+			threshold: 10240,
+			minRatio: 0.8
+		})
+	);
 }
 
-module.exports = config
+module.exports = config;
